f1e07ec543379944990fdaaab0aa61ba454c4786
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2026-07-02 20:33:14 +0700

    feat: initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ext-name
+# status-bar-btn
 
 <a href="https://marketplace.visualstudio.com/items?itemName=antfu.ext-name" target="__blank"><img src="https://badgen.net/vs-marketplace/v/antfu.ext-name?color=333&label=VS%20Code%20Marketplace" alt="Visual Studio Marketplace Version" /></a>
 <a href="https://kermanx.github.io/reactive-vscode/" target="__blank"><img src="https://img.shields.io/badge/made_with-reactive--vscode-%23007ACC?style=flat&labelColor=%23229863"  alt="Made with reactive-vscode" /></a>
@@ -7,7 +7,10 @@
 
 <!-- configs -->
 
-**No data**
+| Key                      | Description                                           | Type      | Default |
+| ------------------------ | ----------------------------------------------------- | --------- | ------- |
+| `status-bar-btn.enabled` | Enable or disable the status bar button.              | `boolean` | `true`  |
+| `status-bar-btn.btns`    | An array of button configurations for the status bar. | `array`   | `[]`    |
 
 <!-- configs -->
 
@@ -15,18 +18,19 @@
 
 <!-- commands -->
 
-**No data**
+| Command                 | Title                                    |
+| ----------------------- | ---------------------------------------- |
+| `status-bar-btn.toggle` | status-bar-btn: Toggle Status Bar Button |
 
 <!-- commands -->
 
-## Sponsors
-
-<p align="center">
-  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
-    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.png'/>
-  </a>
-</p>
-
 ## License
 
-[MIT](./LICENSE.md) License © 2022 [Anthony Fu](https://github.com/antfu)
+This project is licensed under the **Creative Commons
+Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)**
+License.
+
+[![License: CC BY-NC-SA
+4.0](https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png)](https://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+See the **[LICENSE.md](./LICENSE.md)** file for full details.
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,73 @@
-import { defineExtension } from 'reactive-vscode'
-import { window } from 'vscode'
+import type { Command } from 'vscode'
+import { defineExtension, defineLogger, useCommands, useStatusBarItem, watch, watchEffect } from 'reactive-vscode'
+import { ConfigurationTarget, MarkdownString, StatusBarAlignment, ThemeColor } from 'vscode'
+import { config } from './config'
+
+const DEFAULT_BTN_VISIBILITY = true
 
 const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
+  const logger = defineLogger('status-bar-btn')
+
+  watch(() => [config.enabled, config.btns], ([enabled, btns], _, onCleanup) => {
+    if (!enabled || !Array.isArray(btns))
+      return
+
+    const currentBtns = btns.map((btn) => {
+      let tooltip: string | MarkdownString | undefined = btn.tooltip as string | undefined
+      if (btn.tooltip && typeof btn.tooltip === 'object') {
+        const tooltipObj = btn.tooltip as MarkdownString
+        const md = new MarkdownString(tooltipObj.value)
+        md.isTrusted = !!tooltipObj.isTrusted
+        md.supportThemeIcons = !!tooltipObj.supportThemeIcons
+        tooltip = md
+      }
+
+      const item = useStatusBarItem({
+        id: btn.id,
+        alignment: btn.alignment === 'left' ? StatusBarAlignment.Left : StatusBarAlignment.Right,
+        priority: btn.priority,
+        name: btn.name,
+        text: btn.text,
+        tooltip,
+        color: btn.color as string | ThemeColor | undefined,
+        backgroundColor: btn.backgroundColor?.id ? new ThemeColor(btn.backgroundColor.id) : undefined,
+        command: btn.command as string | Command | undefined,
+        accessibilityInformation: btn.accessibilityInformation,
+        visible: btn.visible ?? DEFAULT_BTN_VISIBILITY,
+      })
+
+      watchEffect(() => {
+        logger.appendLine(`Button \"${btn.id ?? btn.name ?? btn.text ?? btn.command}\" visibility changed to: ${btn.visible ?? DEFAULT_BTN_VISIBILITY}`)
+      })
+
+      return item
+    })
+
+    onCleanup(() => {
+      currentBtns.forEach((btn) => {
+        btn.hide()
+        btn.dispose()
+      })
+    })
+  }, { deep: true, immediate: true })
+
+  useCommands({
+    'status-bar-btn.toggle': () => {
+      const inspect = config.inspect('enabled')
+
+      let target = ConfigurationTarget.Global
+      if (inspect?.workspaceValue !== undefined) {
+        target = ConfigurationTarget.Workspace
+      }
+      else if (inspect?.workspaceFolderValue !== undefined) {
+        target = ConfigurationTarget.WorkspaceFolder
+      }
+
+      logger.info(`Toggling status bar button (current value: ${config.enabled})`)
+
+      config.update('enabled', !config.enabled, target)
+    },
+  })
 })
 
 export { activate, deactivate }
